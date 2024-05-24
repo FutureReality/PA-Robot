b9986024123a0dd301b7b7d2839a225765237339
FutureReality <115637305+FutureReality@users.noreply.github.com>
2024-05-24 09:45:04 +0200

    Ya se mueve en todas direcciones

--- robot.java
+++ robot.java
@@ -9,30 +9,34 @@ public class Gui10 extends JFrame {
     JButton derecha;
     JButton guardar;
     JButton cargar;
-	
-	int Y = 0;
-	int X = 0;
-	String direccion = "norte";
+    
+    int Y = 0;
+    int X = 0;
+    String direccion = "norte";
+    JLabel L1;
+    JLabel L2;
 
     public Gui10() {
         setLayout(new FlowLayout());
-        add(new JLabel("Estado del robot: " + X + ", " + Y));
-        add(new JLabel("Direccion: " + direccion));
-		
+        L1 = new JLabel("Estado del robot: " + X + ", " + Y);
+        L2 = new JLabel("Direccion: " + direccion);
+        add(L1);
+        add(L2);
+        
         adelante = new JButton("adelante");
         atras = new JButton("atras");
         izquierda = new JButton("izquierda");
         derecha = new JButton("derecha");
         guardar = new JButton("guardar");
         cargar = new JButton("cargar");
-		
+        
         add(adelante);
         add(atras);
         add(izquierda);
         add(derecha);
         add(guardar);
         add(cargar);
-		
+        
         adelante.addActionListener(new agrega());
         atras.addActionListener(new resta());
         izquierda.addActionListener(new oeste());
@@ -40,7 +44,7 @@ public class Gui10 extends JFrame {
         guardar.addActionListener(new guardarestado());
         cargar.addActionListener(new cargarestado());
 
-        setSize(400, 100);
+        setSize(400, 150);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
@@ -49,49 +53,62 @@ public class Gui10 extends JFrame {
         Gui10 ventana = new Gui10();
     }
 
+    private void actualizarEstado() {
+        L1.setText("Estado del robot: " + X + ", " + Y);
+        L2.setText("Direccion: " + direccion);
+    }
+
     class agrega implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             X++;
-			String direccion = "Norte";
-			System.out.println(X); 
-			System.out.println(direccion); 
+		    direccion = "norte";
+            System.out.println("Direccion: " + direccion);
+            System.out.println("Y: " + Y);
+            actualizarEstado();
         }
     }
 
     class resta implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             X--;
-			System.out.println(X); 
+            direccion = "sur";
+            System.out.println("Direccion: " + direccion);
+            System.out.println("Y: " + Y);
+            actualizarEstado();
         }
     }
-	
+    
     class oeste implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            String direccion = "oeste";
-			System.out.println(direccion); 
+            Y++;
+            direccion = "oeste";
+            System.out.println("Direccion: " + direccion);
+            System.out.println("X: " + X);
+            actualizarEstado();
         }
     }
-	
+    
     class este implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            String direccion = "este";
-			System.out.println(direccion); 
+            Y--;
+            direccion = "este";
+            System.out.println("Direccion: " + direccion);
+            System.out.println("X: " + X);
+            actualizarEstado();
         }
     }
 
     class guardarestado implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            X++;
-			System.out.println(X); 
+            System.out.println("Estado guardado: X=" + X + ", Y=" + Y + ", Direccion=" + direccion);
+			actualizarEstado();
         }
-		
     }
-	
+    
     class cargarestado implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            X++;
-			System.out.println(X); 
+            System.out.println("Estado cargado: X=" + X + ", Y=" + Y + ", Direccion=" + direccion);
+            actualizarEstado();
         }
-		
     }
 }
